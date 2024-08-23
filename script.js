2d08a0b8e6a32872dfca31c0815162ca29f5517b
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2024-08-23 12:28:14 +0530

    Portfolio Project

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('#menu');
+
+const navvar = document.querySelector('.navbar');
+
+menu.addEventListener(onClick,()=>{
+    
+})
+menuIcon.onClick = ()=>{
+    
+    menuIcon.classList.toggle('bx-x');
+    navvar.classList.toggle('active');
+}
